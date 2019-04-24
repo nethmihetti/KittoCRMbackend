f33ed7d2b3cfbef65d3aea716b70ca7493e69e7d
Marat <kashaevmt@gmail.com>
2019-04-24 23:40:10 +0300

    add new route

--- src/routes.py
+++ src/routes.py
@@ -3,6 +3,15 @@ from src import app
 from flask import request
 from flask import abort
 from src.IrohaHandler.transaction import Transaction
+import requests
+
+
+@app.route('/requests', methods=['GET'])
+def app_requests():
+    URL = 'http://34.66.0.246:8080/api/V1/agents/requests?companyId=1&status=ALL'
+    r = requests.get(URL)
+    print(dir(r))
+    return r.text
 
 
 @app.route('/iroha_rest/api/v1.0/items', methods=['POST'])
