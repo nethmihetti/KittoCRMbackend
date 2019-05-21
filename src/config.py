b1ad395f050053ff7e456e4320355bda424fc61a
alxndrtarasov <trezor0210@gmail.com>
2019-05-21 21:11:39 +0300

    Changed Iroha address

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from src.IrohaHandler.transaction import TransactionBuilder
 admin_account = "reg@common"
 admin_private_key = "f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70"
 # !!! change to '127.0.0.1:50051' or your one
-iroha_address = '127.0.0.1:50051'
+iroha_address = '10.90.137.18:50051'
 
 transaction_builder = TransactionBuilder(admin_account, admin_private_key, iroha_address)
 
